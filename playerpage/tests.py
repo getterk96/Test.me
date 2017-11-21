e931ac0ab0f027f744d77f525cba566da9551717
thss15fyt <thss15_fengyt@163.com>
2017-11-21 19:17:12 +0800

    Remodify database User, adapt older back-end to these changes. Ref #54

--- playerpage/tests.py
+++ playerpage/tests.py
@@ -10,23 +10,31 @@ import json
 
 class TestRegister(TestCase):
 
-    def test_register_wrong_type(self):
+    def test_register_wrong_input(self):
         found = resolve('/register', urlconf=playerpage.urls)
         request = Mock(wraps=HttpRequest(), method='POST')
         request.body = Mock()
+        # wrong player type
         request.body.decode = Mock(return_value='{"username":"1", "password":"1",'
-                                                '"email":"1@1.com", "gender":"male",'
-                                                '"playerType":"小学生", "birthday":"2017-11-23"}')
+                                                '"email":"1@1.com", "group": "test_group",'
+                                                '"gender":"male", "playerType":5, "birthday":"2017-11-23"}')
         response = json.loads(found.func(request).content.decode())
-        self.assertEqual(response['code'], 2)
-        self.assertEqual(response['msg'], 'Signup fail')
+        self.assertEqual(response['code'], 1)
+        self.assertEqual(response['msg'], 'Wrong player type')
+        # wrong gender
+        request.body.decode = Mock(return_value='{"username":"2", "password":"2",'
+                                                '"email":"2@2.com", "group": "test_group",'
+                                                '"gender":"unknown","playerType":0, "birthday":"2017-11-23"}')
+        response = json.loads(found.func(request).content.decode())
+        self.assertEqual(response['code'], 1)
+        self.assertEqual(response['msg'], 'Wrong gender')
 
     def test_register_success(self):
         found = resolve('/register', urlconf=playerpage.urls)
         request = Mock(wraps=HttpRequest(), method='POST')
         request.body = Mock()
-        request.body.decode = Mock(return_value='{"username":"2", "password":"2",'
-                                                '"email":"2@2.com", "gender":"male",'
-                                                '"playerType":"研究生", "birthday":"2017-11-23"}')
+        request.body.decode = Mock(return_value='{"username":"3", "password":"3",'
+                                                '"email":"2@2.com", "group": "test_group",'
+                                                '"gender":"male", "playerType":1, "birthday":"2017-11-23"}')
         response = json.loads(found.func(request).content.decode())
         self.assertEqual(response['code'], 0)
--- playerpage/views.py
+++ playerpage/views.py
@@ -1,6 +1,8 @@
 from codex.baseerror import *
 from codex.baseview import APIView
+from codex.basedecorator import *
 from test_me_app.models import *
+from test_me import settings
 
 # Create your views here.
 
@@ -8,22 +10,31 @@ from test_me_app.models import *
 class PlayerRegister(APIView):
 
     def post(self):
-        self.check_input('username', 'password', 'email', 'gender', 'playerType', 'birthday')
+        # check
+        self.check_input('username', 'password', 'email', 'group', 'gender', 'playerType', 'birthday')
+        if self.input['playerType'] < 0 or self.input['playerType'] > 4:
+            raise InputError('Wrong player type')
+        if self.input['gender'] not in ['male', 'female']:
+            raise InputError('Wrong gender')
+        # create
         try:
             user = User.objects.create_user(username=self.input['username'],
                                             password=self.input['password'],
                                             email=self.input['email'])
-            # avatar_url = settings.get_url(settings.STATIC_URL + 'img/default_avatar.jpg')
-            gender = (self.input['gender'] == 'male')
-            for v, k in Players.TYPE_CHOICE:
-                if self.input['playerType'] == k:
-                    player_type = v
-                    break
-
-            player = Players.objects.create(user=user,
-                                            nickname=self.input['username'],
-                                            gender=gender,
-                                            player_type=player_type,
-                                            birthday=self.input['birthday'])
+            user.user_type = User_profile.PLAYER
+            user.save()
+            # default columns
+            avatar_url = settings.get_url(settings.STATIC_URL + 'img/default_avatar.jpg')
+            description = '这个人很懒，什么都没有留下'
+            # create player
+            player = Player.objects.create(user=user,
+                                           nickname=self.input['username'],
+                                           group=self.input['group'],
+                                           avatar_url=avatar_url,
+                                           description=description,
+                                           gender=(self.input['gender'] == 'male'),
+                                           player_type=self.input['playerType'],
+                                           birthday=self.input['birthday'])
+            player.save()
         except:
-            raise LogicError("Signup fail")
+            raise LogicError('Signup fail')
