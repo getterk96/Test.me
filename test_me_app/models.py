e104b668697e1b332b72f2a3056ad3259ff569bf
getterk <gaojh15@mails.tsinghua.edu.cn>
2017-12-01 11:13:11 +0800

    Exception Handlers of Organizers

--- test_me_app/models.py
+++ test_me_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User, UserManager
 from django.db.models.signals import post_save
+from codex.baseerror import *
 
 
 # Create your models here.
@@ -91,6 +92,25 @@ class Contest(models.Model):
     SAVED = 0
     PUBLISHED = 1
 
+    def safeGet(id):
+        try:
+            return Contest.objects.get(id=id)
+        except:
+            raise LogicError("No Such Contest")
+
+    def addTags(self, tags):
+        for content in tags:
+            if not content:
+                continue
+            tag, created = Tag.objects.get_or_create(content=content)
+            if not created:
+                self.save()
+                raise LogicError("Tag Create Error")
+            tag.save()
+            self.tags.add(tag)
+
+        self.save()
+
 
 class Period(models.Model):
     contest = models.ForeignKey(Contest)
@@ -102,6 +122,12 @@ class Period(models.Model):
     description = models.TextField()
     attachment_url = models.CharField(max_length=256)
 
+    def safeGet(id):
+        try:
+            return Period.objects.get(id=id)
+        except:
+            raise LogicError("No Such Period")
+
 
 class ExamQuestion(models.Model):
     period = models.ForeignKey(Period)
@@ -109,6 +135,12 @@ class ExamQuestion(models.Model):
     description = models.TextField()
     attachment_url = models.CharField(max_length=256)
     submission_limit = models.IntegerField()
+
+    def safeGet(id):
+        try:
+            return ExamQuestion.objects.get(id=id)
+        except:
+            raise LogicError("No Such Exam Question")
 
 
 class Team(models.Model):
@@ -125,6 +157,12 @@ class Team(models.Model):
     VERIFYING = 0
     VERIFIED = 1
     DISMISSED = 2
+
+    def safeGet(id):
+        try:
+            return Team.objects.get(id=id)
+        except:
+            raise LogicError("No Such Team")
 
 
 class PeriodScore(models.Model):
