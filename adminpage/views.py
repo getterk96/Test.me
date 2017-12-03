4c03581c717ad333469115a8de2588076c1ccf6f
getterk <gaojh15@mails.tsinghua.edu.cn>
2017-12-03 12:37:27 +0800

    Issue #60 By getterk: Backend Admin Appeal Interface Done

--- adminpage/views.py
+++ adminpage/views.py
@@ -1,4 +1,51 @@
 from codex.baseerror import *
 from codex.baseview import APIView
 
+from codex.basedecorator import admin_required
+from test_me_app.models import Appeal, Contest
+
+
+class AppealDetail(APIView):
+
+    @admin_required
+    def get(self):
+        self.check_input('id')
+        appeal = Appeal.safeGet(self.input['id'])
+        data = {
+            'contestName': appeal.contest.name,
+            'content': appeal.content,
+            'attachmentUrl': appeal.attachment_url,
+            'status': appeal.status,
+        }
+
+        return data
+
+    @admin_required
+    def post(self):
+        self.check_input('id', 'contestId', 'content', 'attachmentUrl', 'status')
+        appeal = Appeal.safeGet(self.input['id'])
+        appeal.target_contest = Contest.safeGet(self.input['contestId'])
+        appeal.target_organizer = appeal.target_contest.organizer
+        appeal.status = self.input['status']
+        appeal.content = self.input['content']
+        appeal.attachment_url = self.input['attachmentUrl']
+        appeal.save()
+
+        return appeal.id
+
+
+class AppealRemove(APIView):
+
+    @admin_required
+    def post(self):
+        self.check_input('id')
+        appeal = Appeal.safeGet(self.input['id'])
+        try:
+            appeal.delete()
+        except:
+            raise LogicError("Appeal Delete Failed")
+
+        return self.input['id']
+
+
 # Create your views here.
