cd9af4a1d55f201509de72c6112898b25a1a74f6
Ica Riluci <ica.riluci@gmail.com>
2017-12-03 13:35:22 +0800

    [ui] initialization of contest detail > the url is /cdetail/index.html > the content exists :   header

---
+++ test_me_view/dist/static/js/cdetail_c.js
@@ -0,0 +1,58 @@
+window.cid = window.get_args('cid');
+
+window.usertype = 1;
+
+const type_p = 0;
+const type_o = 1;
+
+(function () {
+    //PLAYER = 0, ORGANIZER = 1
+    $t('/api/c/user_type', 'GET', {},
+        function (response) {
+            myaccount_c.usertype = response['data'];
+        },
+        function (response) {
+            alert('[' + response.code.toString() + ']' + response.msg);
+        }
+    );
+})();
+
+window.contest = {
+    getAttr : function(qname) {
+        for (i of this.attr)
+            if (i.name == qname)
+                return i.content;
+        console.log('[err] No such attr');
+        return null;
+    },
+    attr : [
+        {
+            name : 'name',
+            content : 'contest 1',
+            editable : true
+        }
+    ],
+    is_guest : false
+};
+
+header.greeting = contest != null ? contest.getAttr('name') : 'Test.Me';
+header.title = '比赛';
+if  (!contest.is_guest && usertype == type_o)
+    header.title += '管理';
+else
+    header.title += '详情';
+
+if (usertype in [0, 1]) {
+    header.link_list.push({
+        alias : '个人中心',
+        link : '../myaccount/index.html',
+        action : empty_f
+    });
+    header.link_list.push({
+        alias : '登出',
+        link : '#',
+        action : function() {
+            logout();
+        }
+    });
+}
---
+++ test_me_view/dist/static/js/uni.js
@@ -0,0 +1,103 @@
+// universal script
+window.my_root_path = 'home/quincy/Work/test.me.view/'
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.withCredentials = true;
+
+// global methods
+window.sendresp404 = function() {
+    var path = window.my_root_path + '404.html?redirect=' + window.location.href;
+    window.location.assign(path);
+};
+
+window.get_args = function(name) {
+    var reg = new RegExp("(^|&)" + name + "=(([^&]*)(&|$))");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return unescape(r[2]);
+    return null;
+};
+
+window.empty_f = function() {};
+//window.site_domain = "http://127.0.0.1:8000";
+
+//wrapped up request sender
+(function(window, undefined) {
+    // This wrapped-up request sender should be initialized with 5 arguments :
+    // > the url requested
+    // > the method of request with choices "POST" and "GET"
+    // > the data to send
+    //   > for "POST", the type of data should be 'FormData'
+    //   > for "GET", the type of data should be a string
+    // > the methods for response
+    //   > proc for success response
+    //   > exception for failure response
+    //   > if no method should be referred, 'window.empty_f' can be used
+    var _$t = window.$t;
+    var _testme = window.testme;
+    var testme = $t = window.$t = window.testme = function(url, method, data, proc, exception) {
+        return new testme.prototype.init(url, method, data, proc, exception);
+    }
+    testme.fn = testme.prototype = {
+        init : function(url, method, data, proc, exception) {
+            if (typeof url == 'string') {
+                /*if (window.XMLHttpRequest) {
+                    t_request = new XMLHttpRequest();
+                    console.log('3!');
+                }
+                else {
+                    alert('Explorer unsupported! Chrome or Firefox suggested. 您的浏览器不再被支持，建议使用Chrome或Firefox浏览器.');
+                    console.log('[err] explorer unsupported');
+                    return undefined;
+                }*/
+                if ((method != 'POST') && (method !='GET')) {
+                    console.log('[err] invlalid method');
+                    return undefined;
+                }
+                try {
+                    data_str = JSON.stringify(data);
+                } catch (e) {
+                    console.log('[err] invalid json syntax');
+                    return undefined
+                }
+                if ((typeof proc != 'function') || (typeof exception != 'function')) {
+                    console.log('[err] \"function\" type required for callback');
+                    return undefined;
+                }
+                if (method == 'POST') {
+                    axios.post(url, data)
+                        .then(function(response) {
+                             if (response['data']['code'] == 0) {
+                                 proc(response['data']);
+                             }
+                             else {
+                                 exception(response['data']);
+                             }
+                        })
+                        .catch(function(error) {
+                             alert('Error occured!');
+                        });
+                }
+                if (method == 'GET') {
+                    axios.get(url, data).
+                        then(function(response) {
+                            if (response['data']['code'] == 0) {
+                                proc(response['data']);
+                            }
+                            else {
+                                exception(response['data']);
+                            }
+                        })
+                        .catch(function(error) {
+                            alert('Error occured!');
+                        });
+                }
+                return this;
+            } else {
+                console.log('[err] \"string\" wanted as url but another type is received.')
+            }
+        }
+    };
+    testme.prototype.init.prototype = testme.prototype;
+}(window, undefined));
+
+const dev = device.default;
