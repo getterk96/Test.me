bf5704c9008cf24ae29d9267415a81807100acdc
getterk <gaojh15@mails.tsinghua.edu.cn>
2017-12-13 08:42:07 +0800

    Issue #63 by getterk: Reusable components for forum added

---
+++ frontend/src/components/forum/others/News.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="'col-md-'+portion">
+    <div class="thumbnail">
+      <img alt="300x200" :src="pic"/>
+      <div class="caption center">
+        <h3>
+          {{title}}
+        </h3>
+        <p>
+          {{content}}
+        </p>
+        <p>
+          <a rel="nofollow" class="btn btn-primary" :href="url">浏览</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'news',
+    props: ['portion', 'pic', 'title', 'content', 'url']
+  }
+</script>
