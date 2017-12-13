c3c9ef91afe61369c4c20dd7b0c87cc95ed01115
getterk <gaojh15@mails.tsinghua.edu.cn>
2017-12-13 08:46:53 +0800

    Issue #63 by getterk: Modify forum list page

--- frontend/src/components/forum/List.vue
+++ frontend/src/components/forum/List.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="list" class="col-md-12 column">
     <list-nav></list-nav>
-    <div class="row clearfix">
-      <list-main portion="10" tn="99+" pn="99+" selected="0"></list-main>
+    <div class="row clearfix main-box">
+      <list-main portion="10" tn="99+" pn="99+" :selected="default_column"></list-main>
       <list-sidebar portion="2"></list-sidebar>
     </div>
     <list-footer></list-footer>
@@ -10,18 +10,23 @@
 </template>
 
 <script>
-  import ListNav from './list/Nav'
+  import Nav from './Nav'
   import ListMain from './list/Main'
-  import ListSidebar from './list/Sidebar'
-  import ListFooter from './list/Footer'
+  import Sidebar from './Sidebar'
+  import Footer from './Footer'
 
   export default {
     name: 'list',
+    data: function () {
+      return {
+        default_column: 0,
+      }
+    },
     components: {
-      'list-nav': ListNav,
+      'list-nav': Nav,
       'list-main': ListMain,
-      'list-sidebar': ListSidebar,
-      'list-footer': ListFooter
+      'list-sidebar': Sidebar,
+      'list-footer': Footer
     }
   }
 </script>
@@ -30,6 +35,7 @@
   .center {
     text-align: center;
   }
+
   .right {
     text-align: right;
   }
