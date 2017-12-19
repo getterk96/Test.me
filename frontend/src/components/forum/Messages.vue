37f1bfd8851894d493d62719b9fdc2683fe0ece7
getterk <gaojh15@mails.tsinghua.edu.cn>
2017-12-19 09:18:46 +0800

    Issue #63 by getterk: Bug shooting

--- frontend/src/components/forum/Messages.vue
+++ frontend/src/components/forum/Messages.vue
@@ -3,49 +3,69 @@
     <h3>
       我的动态
     </h3>
-    <ol>
-      <li>
-        <a id="message-1" href="#">我的帖子被回复了</a>
-      </li>
-      <li>
-        <a id="message-2" href="#">老师给我发了一封私信</a>
-      </li>
-      <li>
-        <a id="message-3" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-4" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-5" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-6" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-7" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-8" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-9" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-10" href="#">？？？</a>
-      </li>
-      <li>
-        <a id="message-others" href="#">其他</a>
-      </li>
+    <ol id='message_list'>
     </ol>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+
   export default {
     name: 'messages',
-    methods: {
-      getMessages: function () {}
+    components: {
+      'message': messageC,
     }
+  };
+
+  let messages = [];
+  let other_url = "";
+
+  let messageC = Vue.extend({
+    name: 'message',
+    template: '<li><a>{{content}}</a></li>',
+    props: ['content']
+  });
+
+  (function () {
+    let url = '/api/p/forum/message';
+    let m = 'GET';
+    let data = {};
+    let success = function (response) {
+      let data = response.data;
+      for (let i = 0; i < data.len; i++) {
+        messages.append({
+          id: "message-" + i,
+          url: data.messages[i].url,
+          title: data.messages[i].title
+        });
+        document.getElementById("#message_list").appendChild(
+          document.createElement("message")
+            .setAttribute("id", messages[i].id)
+            .setAttribute("href", messages[i].url)
+            .setAttribute("content", messages[i].title)
+        )
+      }
+      other_url = data.url;
+      document.getElementById("#message_list").appendChild(
+        document.createElement("message")
+          .setAttribute("id", "other-url")
+          .setAttribute("href", other_url)
+          .setAttribute("content", "其他")
+      );
+    };
+    let failed = function (response) {
+      alert('[' + response.code.toString() + ']' + response.msg);
+    };
+    $t(url, 'GET', {}, success, failed);
+  })();
+  /*
+  api definitions:
+  {
+    len: the message array length no more than 10,
+    messages: [the message array:{title, url: the message detail page url in personal center}],
+    url: personal center message list page url,
   }
+   */
 </script>
+
